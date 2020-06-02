504d1f745169e11348e8bf9d23100ef880699152
Alexandr <sashakobzeww@gmail.com>
2020-06-02 18:37:22 +0300

    .actual desktop

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,6 +1,27 @@
 import Swiper from 'swiper';
 
-var mySwiper = new Swiper('.swiper-container', {
+var slider1 = new Swiper('.swiper-container--1', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 40,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})
+
+
+var slider2 = new Swiper('.swiper-container--3', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
