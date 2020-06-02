245b43983d7edf187e9324a0d888f25b48b25c1a
Alexandr <sashakobzeww@gmail.com>
2020-06-02 21:15:28 +0300

    .near desktop

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,40 +1,40 @@
 import Swiper from 'swiper';
 
 var slider1 = new Swiper('.swiper-container--1', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
     slidesPerView: 3,
     spaceBetween: 40,
-
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     }
 })
 
+var slider2 = new Swiper('.swiper-container--2', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 40,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})
 
-var slider2 = new Swiper('.swiper-container--3', {
-    // Optional parameters
+var slider3 = new Swiper('.swiper-container--3', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 3,
     spaceBetween: 40,
-
-    // If we need pagination
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
