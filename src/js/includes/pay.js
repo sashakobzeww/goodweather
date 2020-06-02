9deb1e727cf23b41040d8788479085569392296b
Alexandr <sashakobzeww@gmail.com>
2020-06-02 14:48:11 +0300

    pay Вкладки переключаются

--- src/js/includes/pay.js
+++ src/js/includes/pay.js
@@ -1,14 +1,17 @@
 let tabsHeaders = document.querySelectorAll('.pay__tabs-header');
 let tabsBorder = document.querySelector('.pay__tabs-border');
+let tabs = document.querySelectorAll('.pay__tab');
 
 for (let i = 0; i < tabsHeaders.length; i++) {
     tabsHeaders[i].onclick = () => {
 
         for (let i = 0; i < tabsHeaders.length; i++) {
             tabsHeaders[i].classList.remove('pay__tabs-header--active')
+            tabs[i].classList.remove('pay__tab--active')
         }
 
         tabsHeaders[i].classList.add('pay__tabs-header--active');
+        tabs[i].classList.add('pay__tab--active')
 
         if (i === 0) {
             tabsBorder.classList.remove('pay__tabs-border--2')
