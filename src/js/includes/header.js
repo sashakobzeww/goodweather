390185bf2207104b5239f71d7b09b647fd5da5e5
Alexandr <sashakobzeww@gmail.com>
2020-06-03 16:49:42 +0300

    .header menu desktop

--- src/js/includes/header.js
+++ src/js/includes/header.js
@@ -1,1 +1,23 @@
-// 
+let menuButton = document.querySelector(".header__menu-button");
+let headerClose = document.querySelector(".header__close");
+
+let headerNav = document.querySelector(".header-nav");
+let headerCenter = document.querySelector(".header__center");
+let headerRight = document.querySelector(".header__right-wrapper");
+let headerTel = document.querySelector(".header__tel");
+
+function toggle() {
+    headerNav.classList.toggle("header-nav--active");
+    headerCenter.classList.toggle("header__center--active");
+    headerRight.classList.toggle("header__right-wrapper--active");
+    headerTel.classList.toggle("header__tel--active");
+    headerClose.classList.toggle("header__close--active");
+};
+
+menuButton.onclick = () => {
+    toggle();
+};
+
+headerClose.onclick = () => {
+    toggle();
+};
