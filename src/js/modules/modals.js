469989c3421973d772464935028e3ade404e042c
Alexandr <sashakobzeww@gmail.com>
2020-06-03 22:14:04 +0300

    .modal desktop .modal-request

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,20 +1,37 @@
-let modal = document.querySelector('.modal--active');
-let modalBackground = document.querySelector('.modal--active .modal__background');
-let modalClose = document.querySelector('.modal--active .modal__close');
-
 let modalRequest = document.querySelector(".modal-request");
-let buttonModalRequest = document.querySelector(".homeNeed__get-link");
+let modalBackground = document.querySelector('.modal-request .modal__background');
+let modalCloseIco = document.querySelector('.modal-request .modal__close');
+let modalRequestForm = document.querySelector(".modal-request__form");
+let modalRequestThanks = document.querySelector(".modal-request-thanks");
+let buttonModalRequest = document.querySelector(".homeNeed__get");
+let modalSend = document.querySelector('.modal-request__form .btn');
+let modalCloseBtn = document.querySelector('.modal-request-thanks .btn');
 
+modalSend.onclick = () => {
+    modalRequestForm.classList.remove('modal-request__form--active');
+    modalRequestThanks.classList.add('modal-request-thanks--active');
+};
 
-modalClose.onclick = () => {
-    modal.classList.remove('modal--active');
+modalCloseIco.onclick = () => {
+    modalClose();
+};
+
+modalCloseBtn.onclick = () => {
+    modalClose();
 };
 
 modalBackground.onclick = () => {
-    modal.classList.remove('modal--active');
+    modalClose();
 };
 
 buttonModalRequest.onclick = (e) => {
-    e.preventDefault();
     modalRequest.classList.add('modal--active');
 };
+
+function modalClose() {
+    modalRequest.classList.remove('modal--active');
+    setTimeout(function () {
+        modalRequestForm.classList.add('modal-request__form--active');
+        modalRequestThanks.classList.remove('modal-request-thanks--active');
+    }, 500)
+}
