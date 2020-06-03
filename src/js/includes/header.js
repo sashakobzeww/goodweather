9debbfde38f501c85c98c40f0ad9000668ed5896
Alexandr <sashakobzeww@gmail.com>
2020-06-03 18:13:23 +0300

    .header quiz desktop

--- src/js/includes/header.js
+++ src/js/includes/header.js
@@ -6,18 +6,31 @@ let headerCenter = document.querySelector(".header__center");
 let headerRight = document.querySelector(".header__right-wrapper");
 let headerTel = document.querySelector(".header__tel");
 
-function toggle() {
-    headerNav.classList.toggle("header-nav--active");
-    headerCenter.classList.toggle("header__center--active");
-    headerRight.classList.toggle("header__right-wrapper--active");
-    headerTel.classList.toggle("header__tel--active");
-    headerClose.classList.toggle("header__close--active");
-};
+let openQuiz = document.querySelector('#openQuiz');
+let headerQuiz = document.querySelector('.header-quiz');
 
 menuButton.onclick = () => {
-    toggle();
+    headerCenter.classList.remove("header__center--active");
+    headerClose.classList.add("header__close--active");
+    headerNav.classList.add("header-nav--active");
+    headerRight.classList.remove("header__right-wrapper--active");
+    headerTel.classList.remove("header__tel--active");
+    headerQuiz.classList.remove("header-quiz--active");
 };
 
 headerClose.onclick = () => {
-    toggle();
+    headerCenter.classList.add("header__center--active");
+    headerClose.classList.remove("header__close--active");
+    headerNav.classList.remove("header-nav--active");
+    headerRight.classList.add("header__right-wrapper--active");
+    headerTel.classList.add("header__tel--active");
+    headerQuiz.classList.remove("header-quiz--active");
+};
+
+openQuiz.onclick = () => {
+    headerCenter.classList.remove("header__center--active");
+    headerClose.classList.add("header__close--active");
+    headerRight.classList.remove("header__right-wrapper--active");
+    headerTel.classList.remove("header__tel--active");
+    headerQuiz.classList.add("header-quiz--active");
 };
