9dbd56e2ef6d8036c212d96114a1e9e26a8ea1a3
Alexandr <sashakobzeww@gmail.com>
2020-06-06 20:04:35 +0300

    Квиз (шапка). Почти готовое решение

--- src/js/includes/header.js
+++ src/js/includes/header.js
@@ -37,3 +37,58 @@ openQuiz.onclick = () => {
     headerQuiz.classList.add("header-quiz--active");
     document.body.classList.add('overflow-hidden')
 };
+
+
+/*Листаем квиз*/
+let headerQuizPrev = document.getElementById('headerQuizPrev');
+let headerQuizNext = document.getElementById('headerQuizNext');
+let headerQuizSteps = document.querySelectorAll(".header-quiz__step");
+
+headerQuizPrev.addEventListener('click', function () {
+    changeQuizStep(-1)
+})
+
+headerQuizNext.addEventListener('click', function () {
+    changeQuizStep(+1)
+})
+
+function changeQuizStep(e) {
+    // Определяем номер активного шага
+    for (let i = 0; i < headerQuizSteps.length; i++) {
+        let has = headerQuizSteps[i].classList.contains('header-quiz__step--active');
+        if (has) var numberOfActiveQuizStep = i;
+    }
+
+    let prevEl = headerQuizSteps[numberOfActiveQuizStep].previousElementSibling;
+    let currentEl = headerQuizSteps[numberOfActiveQuizStep];
+    let nextEl = headerQuizSteps[numberOfActiveQuizStep].nextElementSibling;
+
+    if (currentEl) {
+        headerQuizSteps[numberOfActiveQuizStep].classList.remove('header-quiz__step--active');
+    }
+
+    let abc = numberOfActiveQuizStep + e + 1;
+
+    if (abc === 1) {
+        headerQuizPrev.disabled = true;
+    }
+    if (abc === 2) {
+        headerQuizPrev.disabled = false;
+        headerQuizNext.disabled = false;
+    }
+    if (abc === 3) {
+        headerQuizPrev.disabled = false;
+        headerQuizNext.disabled = false;
+    }
+    if (abc === 4) {
+        headerQuizNext.disabled = true;
+    }
+
+    if (e > 0 && nextEl) {
+        nextEl.classList.add('header-quiz__step--active');
+    } else if (e < 0 && prevEl) {
+        prevEl.classList.add('header-quiz__step--active');
+    }
+
+}
+
