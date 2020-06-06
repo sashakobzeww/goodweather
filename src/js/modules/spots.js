6eb213d723f65efb77c395102000e06d184f0c65
Alexandr <sashakobzeww@gmail.com>
2020-06-06 11:39:12 +0300

    Анимации: срабатывает при прокрутке (на примере одной секции)

--- src/js/modules/spots.js
+++ src/js/modules/spots.js
@@ -4,11 +4,38 @@ let spotTriggers = document.querySelectorAll('[id^="spot-trigger"]');
 for (let i = 0; i < spotTriggers.length; i++) {
 
     spotTriggers[i].addEventListener('mouseover', function () {
-        spots[i].classList.add("scale-in-center")
+        spots[i].classList.add("scale-in-center-transform")
     });
 
     spotTriggers[i].addEventListener('mouseout', function () {
-        spots[i].classList.remove("scale-in-center")
+        spots[i].classList.remove("scale-in-center-transform")
     });
 
+}
+
+/*----*/
+
+document.addEventListener("DOMContentLoaded", scrolling, false);
+
+let listItems = document.querySelectorAll(".infographics");
+
+function scrolling(e) {
+
+    for (let i = 0; i < listItems.length; i++) {
+        let listItem = listItems[i];
+
+        if (isPartiallyVisible(listItem)) {
+            listItem.classList.add("animation");
+        }
+    }
+}
+
+function isPartiallyVisible(el) {
+    let elementBoundary = el.getBoundingClientRect();
+
+    let top = elementBoundary.top;
+    let bottom = elementBoundary.bottom;
+    let height = elementBoundary.height;
+
+    return ((top + height >= 0) && (height + window.innerHeight >= bottom));
 }
