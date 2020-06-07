bf531dfdfdbcfd3b4eb83ba687a7f00a07606cff
Alexandr <sashakobzeww@gmail.com>
2020-06-07 17:58:47 +0300

    mobile (max-width: 1024px) .sliders

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -2,8 +2,6 @@ import Swiper from 'swiper';
 
 var slider1 = new Swiper('.swiper-container--1', {
     direction: 'horizontal',
-    slidesPerView: 3,
-    spaceBetween: 40,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -11,16 +9,38 @@ var slider1 = new Swiper('.swiper-container--1', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+    spaceBetween: 40,
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
     }
 })
 
 var slider2 = new Swiper('.swiper-container--2', {
     direction: 'horizontal',
-    slidesPerView: 4,
-    spaceBetween: 40,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 4,
+    spaceBetween: 40,
+    breakpoints: {
+        320: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
     }
 })
 
@@ -35,6 +55,16 @@ var slider3 = new Swiper('.swiper-container--3', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
     }
 })
 
@@ -49,13 +79,21 @@ var slider41 = new Swiper('.swiper-container--4-1', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
     }
 })
 
 var slider42 = new Swiper('.swiper-container--4-2', {
     direction: 'horizontal',
-    slidesPerView: 3,
-    spaceBetween: 40,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -63,5 +101,17 @@ var slider42 = new Swiper('.swiper-container--4-2', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+    spaceBetween: 40,
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
     }
 })
