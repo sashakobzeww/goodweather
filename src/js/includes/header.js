aa41d4f90fdbb900ab00ba3050798d1121c4b236
Alexandr <sashakobzeww@gmail.com>
2020-06-11 10:36:20 +0300

    header mob Скрытие кнопки меню при открытии меню или квиза

--- src/js/includes/header.js
+++ src/js/includes/header.js
@@ -1,14 +1,16 @@
+/* Элементы управления */
 let menuButton = document.querySelector(".header__menu-button");
 let headerClose = document.querySelector(".header__close");
+let openQuiz = document.querySelector('#openQuiz');
 
+/* Скрываемые и появляемые элементы */
 let headerNav = document.querySelector(".header-nav");
 let headerCenter = document.querySelector(".header__center");
 let headerRight = document.querySelector(".header__right-wrapper");
 let headerTel = document.querySelector(".header__tel");
-
-let openQuiz = document.querySelector('#openQuiz');
 let headerQuiz = document.querySelector('.header-quiz');
 
+/* Открыть меню */
 menuButton.onclick = () => {
     headerCenter.classList.remove("header__center--active");
     headerClose.classList.add("header__close--active");
@@ -16,9 +18,26 @@ menuButton.onclick = () => {
     headerRight.classList.remove("header__right-wrapper--active");
     headerTel.classList.remove("header__tel--active");
     headerQuiz.classList.remove("header-quiz--active");
-    document.body.classList.remove('overflow-hidden')
+    document.body.classList.remove('overflow-hidden');
+    if (window.innerWidth < 768) {
+        menuButton.classList.remove("header__menu-button--active")
+    }
 };
 
+/* Открыть квиз */
+openQuiz.onclick = () => {
+    headerCenter.classList.remove("header__center--active");
+    headerClose.classList.add("header__close--active");
+    headerRight.classList.remove("header__right-wrapper--active");
+    headerTel.classList.remove("header__tel--active");
+    headerQuiz.classList.add("header-quiz--active");
+    document.body.classList.add('overflow-hidden');
+    if (window.innerWidth < 768) {
+        menuButton.classList.remove("header__menu-button--active")
+    }
+};
+
+/* Закрыть меню или квиз */
 headerClose.onclick = () => {
     headerCenter.classList.add("header__center--active");
     headerClose.classList.remove("header__close--active");
@@ -26,14 +45,6 @@ headerClose.onclick = () => {
     headerRight.classList.add("header__right-wrapper--active");
     headerTel.classList.add("header__tel--active");
     headerQuiz.classList.remove("header-quiz--active");
-    document.body.classList.remove('overflow-hidden')
-};
-
-openQuiz.onclick = () => {
-    headerCenter.classList.remove("header__center--active");
-    headerClose.classList.add("header__close--active");
-    headerRight.classList.remove("header__right-wrapper--active");
-    headerTel.classList.remove("header__tel--active");
-    headerQuiz.classList.add("header-quiz--active");
-    document.body.classList.add('overflow-hidden')
+    document.body.classList.remove('overflow-hidden');
+    menuButton.classList.add("header__menu-button--active")
 };
