dd98d4f7832ad1c7f4dd6d4f920e0b8fbcfe92d4
Alexandr <sashakobzeww@gmail.com>
2020-06-11 13:07:51 +0300

    plans Рефакторинг

--- src/js/includes/plans.js
+++ src/js/includes/plans.js
@@ -1,5 +1,5 @@
-let tabsHeadersLiter = document.querySelectorAll('.plans__tabs-headers--liter .plans__tabs-header');
 let tabsHeadersPorch = document.querySelectorAll('.plans__tabs-headers--porch .plans__tabs-header');
+let tabsHeadersLiter = document.querySelectorAll('.plans__tabs-headers--liter .plans__tabs-header');
 let tabs = document.querySelectorAll(".plans__tab");
 
 let tabs11 = document.querySelector(".plans__tab--1-1");
@@ -9,29 +9,22 @@ let tabs21 = document.querySelector(".plans__tab--2-1");
 let tabs22 = document.querySelector(".plans__tab--2-2");
 let tabs23 = document.querySelector(".plans__tab--2-3");
 
-var a;
-var b;
+var currentTabPorch;
+var currentTabLitter;
 
 for (let i = 0; i < tabsHeadersLiter.length; i++) {
     tabsHeadersLiter[i].onclick = () => {
         for (let i = 0; i < tabs.length; i++)  {
-            tabs[i].classList.remove('plan__tab--active-liter')
+            tabs[i].classList.remove('plan__tab--active-liter');
             if (tabsHeadersLiter[i] !== undefined) {
                 tabsHeadersLiter[i].classList.remove('plans__tabs-header--active')
             }
         }
 
-        if (i === 0) {
-            tabsHeadersLiter[i].classList.add('plans__tabs-header--active');
-            a = i;
-        }
+        tabsHeadersLiter[i].classList.add('plans__tabs-header--active');
+        currentTabPorch = i + 1;
 
-        if (i === 1) {
-            tabsHeadersLiter[i].classList.add('plans__tabs-header--active');
-            a = i;
-        }
-
-        changeTab(a, b);
+        changeTab(currentTabPorch, currentTabLitter);
 
     };
 }
@@ -39,65 +32,44 @@ for (let i = 0; i < tabsHeadersLiter.length; i++) {
 for (let i = 0; i < tabsHeadersPorch.length; i++) {
     tabsHeadersPorch[i].onclick = () => {
         for (let i = 0; i < tabs.length; i++)  {
-            tabs[i].classList.remove('plan__tab--active-porch')
+            tabs[i].classList.remove('plan__tab--active-porch');
             if (tabsHeadersPorch[i] !== undefined) {
                 tabsHeadersPorch[i].classList.remove('plans__tabs-header--active')
             }
         }
 
-        if (i === 0) {
-            tabsHeadersPorch[i].classList.add('plans__tabs-header--active');
-            b = i;
-        }
+        tabsHeadersPorch[i].classList.add('plans__tabs-header--active');
+        currentTabLitter = i + 1;
 
-        if (i === 1) {
-            tabsHeadersPorch[i].classList.add('plans__tabs-header--active');
-            b = i;
-        }
-
-        if (i === 2) {
-            tabsHeadersPorch[i].classList.add('plans__tabs-header--active');
-            b = i;
-        }
-
-        changeTab(a, b);
+        changeTab(currentTabPorch, currentTabLitter);
     };
 }
 
-function changeTab (a, b) {
-    if (a === undefined) {
-        a = 0
-    }
-    if (b === undefined) {
-        b = 0
-    }
-    console.log({a, b});
+function changeTab (currentTabPorch, currentTabLitter) {
+    currentTabPorch === undefined ? currentTabPorch = 1 : currentTabPorch = currentTabPorch;
+    currentTabLitter === undefined ? currentTabLitter = 1 : currentTabLitter = currentTabLitter;
 
     for (let i = 0; i < tabs.length; i++) {
         tabs[i].classList.remove('plans__tab--active');
     }
 
-    if (a === 0 && b === 0) {
+    if (currentTabPorch === 1 && currentTabLitter === 1) {
         tabs11.classList.add('plans__tab--active');
     }
-
-    if (a === 1 && b === 1) {
-        tabs22.classList.add('plans__tab--active');
-    }
-
-    if (a === 2 && b === 0) {
-        tabs13.classList.add('plans__tab--active');
-    }
-
-    if (a === 0 && b === 1) {
+    if (currentTabPorch === 1 && currentTabLitter === 2) {
         tabs12.classList.add('plans__tab--active');
     }
-
-    if (a === 1 && b === 0) {
+    if (currentTabPorch === 1 && currentTabLitter === 3) {
+        tabs13.classList.add('plans__tab--active');
+    }
+    if (currentTabPorch === 2 && currentTabLitter === 1) {
         tabs21.classList.add('plans__tab--active');
     }
-
-    if (a === 2 && b === 1) {
+    if (currentTabPorch === 2 && currentTabLitter === 2) {
+        tabs22.classList.add('plans__tab--active');
+    }
+    if (currentTabPorch === 2 && currentTabLitter === 3) {
         tabs23.classList.add('plans__tab--active');
     }
+
 }
