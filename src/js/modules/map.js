2f43ae85c6ab18fd958bdbe1636192ea78268521
Alexandr <sashakobzeww@gmail.com>
2020-06-13 17:07:07 +0300

    Поставлены метки на карту

--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,11 +1,41 @@
-import ymaps from 'ymaps';
+ymaps.ready(initMap);
 
-ymaps
-    .load()
-    .then(maps => {
-        const map = new maps.Map('map', {
-            center: [45.092098, 39.016797],
-            zoom: 15
-        });
-    })
-    .catch(error => console.log('Failed to load Yandex Maps', error));
+function initMap() {
+
+    var myMap = new ymaps.Map(
+        "map", {
+            center: [45.091644, 39.020081],
+            zoom: 15,
+            height: 520,
+            controls: []
+        }
+    );
+
+    myMap.geoObjects.add(
+        new ymaps.Placemark(
+            [45.087191, 39.016463], {
+                balloonContent: '',
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: './img/map__1.png',
+                iconImageSize: [200, 200],
+                iconImageOffset: [-100,-150],
+                zIndex: 2
+            }
+        )
+    );
+
+    myMap.geoObjects.add(
+        new ymaps.Placemark(
+            [45.090678, 39.025003], {
+                balloonContent: '',
+            }, {
+                iconLayout: 'default#image',
+                iconImageHref: './img/map__2.svg',
+                iconImageSize: [200, 200],
+                iconImageOffset: [-100,-150],
+                zIndex: 2
+            }
+        )
+    );
+};
