3bbf049e51f38b2aeb94e2ba98b0786ad94ca05c
Alexandr <sashakobzeww@gmail.com>
2020-06-17 18:14:23 +0300

    Правки pay desktop

--- src/js/includes/pay.js
+++ src/js/includes/pay.js
@@ -92,6 +92,7 @@ payYearInput.addEventListener('change', function () {
 let tabsHeaders = document.querySelectorAll('.pay__tabs-header');
 let tabsBorder = document.querySelector('.pay__tabs-border');
 let tabs = document.querySelectorAll('.pay__tab');
+let payTabList = document.querySelector('.pay__tab-list');
 
 for (let i = 0; i < tabsHeaders.length; i++) {
     tabsHeaders[i].onclick = () => {
@@ -107,17 +108,20 @@ for (let i = 0; i < tabsHeaders.length; i++) {
         if (i === 0) {
             tabsBorder.classList.remove('pay__tabs-border--2')
             tabsBorder.classList.remove('pay__tabs-border--3')
+            payTabList.classList.remove('pay__tab-list--height-1');
             tabsBorder.classList.add('pay__tabs-border--1');
         }
         if (i === 1) {
             tabsBorder.classList.remove('pay__tabs-border--1')
             tabsBorder.classList.remove('pay__tabs-border--3')
             tabsBorder.classList.add('pay__tabs-border--2');
+            payTabList.classList.add('pay__tab-list--height-1');
         }
         if (i === 2) {
             tabsBorder.classList.remove('pay__tabs-border--1')
             tabsBorder.classList.remove('pay__tabs-border--2')
             tabsBorder.classList.add('pay__tabs-border--3');
+            payTabList.classList.add('pay__tab-list--height-1');
         }
 
     }
