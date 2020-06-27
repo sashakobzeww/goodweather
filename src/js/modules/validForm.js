f0d0f17790c28d1fc2540971f0f1e557aacff67c
Alexandr <sashakobzeww@gmail.com>
2020-06-27 11:24:43 +0300

    Валидация Рефакторинг

--- src/js/modules/validForm.js
+++ src/js/modules/validForm.js
@@ -4,78 +4,41 @@ for (let i = 0; i < forms.length; i++) {
     let form = forms[i];
     let send = form.querySelector('.btn');
     let inputs = form.querySelectorAll('[required]');
-    let personalData = form.querySelector('.personal-data__input');
 
     for (let j = 0; j < inputs.length; j++) {
-        inputs[j].addEventListener('keyup', () => {
-            let valid = validTextInputs(inputs, personalData);
-            if (valid) {
-                send.disabled = false;
-            } else {
+
+        if (inputs[j].tagName === 'TEXTAREA' || inputs[j].type === 'text' || inputs[j].type === 'tel') {
+            inputs[j].addEventListener('keyup', () => valid(inputs, send));
+        }
+
+        if (inputs[j].classList.contains('personal-data__input')) {
+            inputs[j].addEventListener('change', () => valid(inputs, send));
+        }
+    }
+}
+
+function valid(inputs, send) {
+
+    for (let i = 0; i < inputs.length; i++) {
+
+        if (inputs[i].tagName === 'TEXTAREA' || inputs[i].type === 'text') {
+            if (inputs[i].textLength === 0) {
                 send.disabled = true;
+                break
             }
-        });
-
-    }
-    personalData.addEventListener('change', () => {
-        let valid = validPersonalData(inputs, personalData);
-        if (valid) {
-            send.disabled = false;
-        } else {
+        } else if (inputs[i].type === 'tel') {
+            let lastSymbol = inputs[i].value[inputs[i].value.length - 1];
+            if (lastSymbol === '_') {
+                send.disabled = true;
+                break
+            }
+        } else if (inputs[i].classList.contains('personal-data__input') && inputs[i].checked === false) {
             send.disabled = true;
-        }
-    })
-
-}
-
-function validTextInputs(inputs, personalData) {
-
-    for (let j = 0; j < inputs.length; j++) {
-        let input = inputs[j];
-
-        if (input.tagName === 'TEXTAREA') {
-            if (input.textLength === 0) {
-                return false
-            }
+            break
+        } else {
+            send.disabled = false;
         }
 
-        if (input.type === 'tel') {
-            let lastSymbol = input.value[input.value.length-1];
-            if (lastSymbol === '_') {
-                return false
-            }
-        }
     }
 
-    if (validPersonalData(inputs, personalData)) {
-        return true
-    }
-
-    return true
-}
-
-function validPersonalData(inputs, personalData) {
-
-    for (let j = 0; j < inputs.length; j++) {
-        let input = inputs[j];
-
-        if (input.tagName === 'TEXTAREA') {
-            if (input.textLength === 0) {
-                return false
-            }
-        }
-
-        if (input.type === 'tel') {
-            let lastSymbol = input.value[input.value.length-1];
-            if (lastSymbol === '_') {
-                return false
-            }
-        }
-    }
-
-    if (personalData.checked) {
-        return true
-    } else {
-        return false
-    }
 }
